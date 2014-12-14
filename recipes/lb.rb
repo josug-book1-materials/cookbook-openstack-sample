ec3537d6a3742ec2685d9e3660c89a4206b1affc
Nobuteru Nishida <nobuteru.nishida@ctc-g.co.jp>
2014-12-14 14:41:32 +0000

    delete default.conf

--- recipes/lb.rb
+++ recipes/lb.rb
@@ -11,7 +11,11 @@ include_recipe "nginx::default"
 template "#{node['nginx']['dir']}/sites-available/default" do
   source "default-site.erb"
   owner "root"
-  group "root"
+  group node['root_group']
   mode 00644
+end
+
+file "#{node['nginx']['dir']}/conf.d/default.conf" do
+  action :delete
   notifies :reload, 'service[nginx]'
 end

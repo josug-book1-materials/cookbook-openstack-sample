2f5eab6368e5122712537b33391c613f1e8ad383
Nobuteru Nishida <nobuteru.nishida@ctc-g.co.jp>
2014-12-14 11:15:26 +0000

    fix typo

--- attributes/role_hosts.rb
+++ attributes/role_hosts.rb
@@ -1,4 +1,4 @@
-default['sample-app']['db_host'] = ["172.16.10.1"]
-default['sample-app']['rest_host'] = ["172.16.10.2"]
-default['sample-app']['web_host'] = ["172.16.10.3", "172.168.10.4"]
-default['sample-app']['lb_host'] = ["172.16.10.5"]
+default['sample-app']['db_hosts'] = ["172.16.10.1"]
+default['sample-app']['rest_hosts'] = ["172.16.10.2"]
+default['sample-app']['web_hosts'] = ["172.16.10.3", "172.168.10.4"]
+default['sample-app']['lb_hosts'] = ["172.16.10.5"]

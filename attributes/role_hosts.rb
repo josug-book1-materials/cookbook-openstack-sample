0173e68bb96eac491a34fdd637d7f14318f18252
Nobuteru Nishida <nobuteru.nishida@ctc-g.co.jp>
2014-12-14 12:35:31 +0000

    rename recipe

--- attributes/role_hosts.rb
+++ attributes/role_hosts.rb
@@ -1,4 +1,4 @@
 default['sample-app']['db_hosts'] = ["172.16.10.1"]
-default['sample-app']['rest_hosts'] = ["172.16.10.2"]
+default['sample-app']['app_hosts'] = ["172.16.10.2"]
 default['sample-app']['web_hosts'] = ["172.16.10.3", "172.168.10.4"]
 default['sample-app']['lb_hosts'] = ["172.16.10.5"]
